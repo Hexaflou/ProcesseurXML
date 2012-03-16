7608c99ff2822be0d3cfaf90113196a7a8cc8459
Olivier EBLE <olivier.eble@insa-lyon.fr>
2012-03-16 09:06:15 +0100

    add constructors prototypes for the xml structure (cpp)

--- AnalyseurXML/cdata.h
+++ AnalyseurXML/cdata.h
@@ -1,4 +1,19 @@
 #ifndef CDATA_H
 #define CDATA_H
 
+#include <string>
+#include "xmlelement.h"
+#include "xmlnode.h"
+
+class Cdata : public XmlElement{
+	
+	std::string content;
+	
+	public :
+	Cdata(string content, XmlNode * parent);
+	
+	//Getters
+	virtual string toString();
+}
+
 #endif

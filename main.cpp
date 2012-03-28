60572824fef5b567234642e987d8726c60d9b8b8
Stanislas Signoud <signez@stanisoft.net>
2012-03-28 20:16:32 +0200

    Adding support for manually passing rules in command-line.

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	int codeRetour = 0; // Code d'erreur
 	
 	// Assez d'arguments ?
-	if (argc == 2) {
+	if (argc > 1 && argc < 4) {
 		// Construction du document XML
 		Document * thedoc = new Document(string(argv[1]));
 		// Lancement du parseur XML
@@ -39,11 +39,14 @@ int main(int argc, char *argv[])
 			// Lancement de la validation XML
 			if (thedoc->isValid()) {
 				cout << "[Xylo] Le document spécifié est valide." << endl;
-/*				Document * thedocxslt = new Document(string(argv[2]));
-				thedocxslt->parse();
-				Document * newdocxslt = thedocxslt->transformToXsltTree();
-				delete thedocxslt;
-				thedoc->toTree(newdocxslt);*/
+				
+				if(argc == 3) { // Fichier XSLT spécifié ?
+					Document * thedocxslt = new Document(string(argv[2]));
+					thedocxslt->parse();
+					Document * newdocxslt = thedocxslt->transformToXsltTree();
+					delete thedocxslt;
+					thedoc->toTree(newdocxslt);
+				}
 			} else {
 				cout << "[Xylo] La document spécifié n'est pas valide." << endl;
 				codeRetour = 0;
@@ -56,8 +59,13 @@ int main(int argc, char *argv[])
 		// Pas assez d'arguments : expliquons le fonctionnement du programme
 		cout << "Xylo XML analyser (0.1b) - 2012, by Hexanet" << endl;
 		
-		cout << "usage: " << argv[0] << " fichier.xml" << endl;
-		cout << "E: Aucun fichier passé en argument : rien à faire." << endl;
+		cout << "usage: " << argv[0] << " file.xml [rules.xsl]" << endl;
+		
+		if(argc <= 1) {
+			cout << "E: Aucun fichier passé en argument : rien à faire." << endl;
+		}else{
+			cout << "E: Trop d'arguments ; référez-vous à l'usage décrit ci-dessus." << endl;
+		}
 	}
 	
 	return codeRetour;

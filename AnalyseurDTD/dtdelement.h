34380e5409b8d0e856b62d78ada5a0101647220f
Stanislas Signoud <signez@stanisoft.net>
2012-03-28 22:49:16 +0200

    Documenting DtdElement.

--- AnalyseurDTD/dtdelement.h
+++ AnalyseurDTD/dtdelement.h
@@ -7,47 +7,76 @@
 #include <map>
 
 #include  "dtdattribute.h"
-typedef std::map<std::string,std::string> AttMap;
+typedef std::map<std::string, std::string> AttMap;
 
+/**
+ * Représente la spécification d'un élément au sein d'une DTD.
+ */
 class DtdElement {
-
 	std::string name;
-	
-	//remarque c'est un set d'attributs, dans le cadre 
-	//d'une evolutivité il faudrai remplacer ce string 
-	//par un objet de type attribut
-	//TODO: Réfléchir à l'utilisation d'un map au lieu d'un set...
-	
+
 	typedef std::set<DtdAttribute> AttSet;
 	AttSet attributs;
-	//for the return of an insert in the attribute list.
-	typedef std::pair<AttSet::iterator,bool> RetAttInsert;
-	
-	//directement tiree du fichier le pattern regex
+	// Pour le retour des insertions dans le set des attributs
+	typedef std::pair<AttSet::iterator, bool> RetAttInsert;
+
+	// Motif directement tiré du fichier, avec une syntaxe DTDienne
 	std::string regexPattern;
-	 
-	public :
-		DtdElement(std::string name);
-		bool addAttribute(std::string const & name);
-		
-		std::string getName() const;
-		
-		//retourne l'élément sous forme de chaine.
-		std::string toString() const;
-		std::string attributeListToString() const;
-		
-		void completeChildPattern(std::string r);//TODO:c'est tout pour le moment
-		
-		bool validate(AttMap attributes) const;
-		bool validate(std::string childrenNames) const;
-		
-		//TODO:implementer les fonctions de verification
-		//verifie si la liste de fils (sous forme de string séparé par des virgules)
-		//récupéré depuis un xml est conforme à la dtd. retourne vrai si valide.
-		//bool checkXMLChildren(std::string liste);
+
+public:
+	/**
+	 * Construit la spécification d'un élement XML au sein d'une DTD.
+     * @param name Le nom de l'élément.
+     */
+	DtdElement(std::string name);
+	
+	/**
+	 * Ajoute un attribut à l'élément.
+     * @param name Nom de l'attribut.
+     * @return True si l'attribut a bien été ajouté, false dans le cas
+	 * contraire (attribut déjà existant par exemple).
+     */
+	bool addAttribute(std::string const & name);
+
+	std::string getName() const;
+
+	/**
+	 * Retourne une représentation sous forme de chaîne de l'élément DTD.
+     * @return Une chaîne représentant l'élément DTD.
+     */
+	std::string toString() const;
+	
+	/**
+	 * Retourne la liste des attributs sous la forme d'un extrait de DTD.
+     * @return Une chaîne représentant les attributs DTD de l'élément.
+     */
+	std::string attributeListToString() const;
+
+	/**
+	 * Ajoute la chaîne r à la fin du motif de conformité des enfants.
+     * @param r Chaîne à ajouter à la fin du motif.
+     */
+	void completeChildPattern(std::string r);
+
+	/**
+	 * Valide la conformité entre les attributs passés en paramètres et
+	 * ceux que peuvent accepter l'élément.
+     * @param attributes Les attributs à valider.
+     * @return true si les attributs sont valides, false sinon.
+     */
+	bool validate(AttMap attributes) const;
+	
+	/**
+	 * Valide la conformité de la liste des enfants passés en paramètre
+	 * avec le motif des enfants que peut contenir l'élément.
+     * @param childrenNames Une chaîne contenant la liste des enfants
+	 * du noeud à vérifier.
+     * @return true si c'est valide, false sinon.
+     */
+	bool validate(std::string childrenNames) const;
 };
 
-//surcharge d'opérateur pour utilisation dans un set
+// Surcharges d'opérateurs diverses, pour l'utilisation dans un set
 bool operator<(DtdElement const &a, DtdElement const& b);
 bool operator<=(DtdElement const &a, DtdElement const& b);
 bool operator>(DtdElement const &a, DtdElement const& b);
